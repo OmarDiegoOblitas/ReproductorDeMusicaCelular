0cae410a531e452248305e3647f3a5295c85df4f
OmarDiegoOblitas <51056602+OmarDiegoOblitas@users.noreply.github.com>
2021-11-01 15:54:24 -0500

    Reproductor de musica para celulares

---
+++ js/LIstMusic.js
@@ -0,0 +1,44 @@
+let allMusic = [
+    {
+        name : "Ex Convicto",
+        artist :"Galante El Emperador" ,
+        img: "img4",
+        src: "Galante El Emperador - Ex Convicto (WWW.ELGENERO.COM)"
+    },
+
+    {
+        name : "Aprovecha Remix",
+        artist :"Anonimus" ,
+        img: "img1",
+        src: "Anonimus, Ozuna, Juhn El All Star, Justin Quiles, Juanka El Problematik - Aprovecha Remix"
+    },
+
+    {
+        name : "Ex Convicto",
+        artist :"Galante El Emperador" ,
+        img: "img4",
+        src: "Galante El Emperador - Ex Convicto (WWW.ELGENERO.COM)"
+    },
+
+    {
+        name : "Alto Contenido (Remix)",
+        artist :"Maldy" ,
+        img: "img1",
+        src: "Maldy Ft. Randy, Luigi 21 Plus, Chencho, Jowell & Ñejo – Alto Contenido (Remix)"
+    },
+
+    {
+        name : "Pura-Falsedad",
+        artist :"Mark-B" ,
+        img: "img3",
+        src: "Mark-B-Ft.-Farruko-Justin-Quiles-Y-Kevin-Roldan-Pura-Falsedad"
+    },
+
+    {
+        name : "Solo-Vivo-Para-Amar-Version-Mambo",
+        artist :"Omega-El-Fuerte" ,
+        img: "img4",
+        src: "Omega-El-Fuerte-–-Solo-Vivo-Para-Amar-Version-Mambo"
+    }
+    
+]
